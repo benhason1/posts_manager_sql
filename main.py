58f5574a68a005ff206625a301a2fb4e377532f0
Ben <hason.ben2@gmail.com>
2020-09-20 17:51:29 +0300

    removed redudant lines from main

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from abc import ABC, abstractmethod
-
-
-class Parent(ABC):
-    # @abstractmethod
-    def foo(self):
-        print("ben")
-
-
-class Child(Parent):
-    def foo(self):
-        print("ben")
-
-
 def main():
-    a = Parent()
-    a.foo()
-
+    pass
 
 if __name__ == "__main__":
     main()
