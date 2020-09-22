c5265b33b5a070c66e6e0ef711593a31f01b342a
Ben <hason.ben2@gmail.com>
2020-09-22 08:51:52 +0300

    added logger

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import logging
+from dynaconf import settings
+
 def main():
-    pass
+    logger = logging.getLogger()
+    if settings.get("LOG_LEVEL") is None:
+        log_level = "ERROR"
+    else:
+        log_level = settings.LOG_LEVEL
+
+    logger.setLevel(log_level)
 
 if __name__ == "__main__":
     main()
